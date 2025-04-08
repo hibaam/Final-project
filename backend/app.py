4eacca01272d6990b1250168c92ca0522e0e957e
hibaam <hibajeje@gmail.com>
2025-04-08 17:28:31 +0300

    pushing the BackEnd

--- backend/app.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-import yt_dlp
-import openai
-import os
-
-from dotenv import load_dotenv
