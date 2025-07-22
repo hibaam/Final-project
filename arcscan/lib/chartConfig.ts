cdc60db785dcda157e8a87184d75c4246fc37e31
hibaam <hibajeje@gmail.com>
2025-07-22 05:08:33 +0300

    Update file: arcscan/lib/chartConfig.ts

---
+++ arcscan/lib/chartConfig.ts
@@ -0,0 +1,19 @@
+// This file registers Chart.js components globally
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
